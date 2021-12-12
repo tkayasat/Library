4ab9e2ebaaa5400ced1e6435502ffeb246e6058c
tkaya <tkayasat@gmail.com>
2021-12-12 22:51:49 +0300

    дз 5 добавлены -      Retrofit в loadData в UseresPresenter редактировано  - GithubUserRepository

--- app/src/main/java/com/example/library/newproject_cicerone/model/domain/GithubUsersRepository.kt
+++ app/src/main/java/com/example/library/newproject_cicerone/model/domain/GithubUsersRepository.kt
@@ -1,18 +1,10 @@
 package com.example.library.newproject_cicerone.model.domain
 
 import com.example.library.newproject_cicerone.model.GithubUserModel
-import io.reactivex.rxjava3.core.Single
 
 class GithubUsersRepository {
-    private val users = listOf(
-        GithubUserModel("Login 1"),
-        GithubUserModel("Login 2"),
-        GithubUserModel("Login 3"),
-        GithubUserModel("Login 4"),
-        GithubUserModel("Login 5"),
-    )
 
-    fun getUsers(): Single<List<GithubUserModel>> {
-        return Single.just(users)
+    fun getUsers(): List<GithubUserModel> {
+        return emptyList()
     }
 }
--- app/src/main/java/com/example/library/newproject_cicerone/ui/ui/users/UsersPresenter.kt
+++ app/src/main/java/com/example/library/newproject_cicerone/ui/ui/users/UsersPresenter.kt
@@ -3,9 +3,12 @@ package com.example.library.newproject_cicerone.ui.ui.users
 import android.util.Log
 import com.example.library.newproject_cicerone.model.GithubUserModel
 import com.example.library.newproject_cicerone.model.domain.GithubUsersRepository
+import com.example.library.newproject_cicerone.remote.ApiHolder
 import com.example.library.newproject_cicerone.screens.AppScreens
 import com.github.terrakok.cicerone.Router
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import moxy.MvpPresenter
 import okhttp3.*
 import java.io.IOException
@@ -26,7 +29,15 @@ class UsersPresenter(
     }
 
     private fun loadData() {
-        val okHttpClient= OkHttpClient.Builder().build()
+        val service = ApiHolder.retrofitService
+        service.getUsers()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { users: List<GithubUserModel> ->
+                Log.d("Retrofit", "Первый пользователь: ${users.firstOrNull()?.login}")
+
+            //#region  okHttpClient - разобрано
+                /*val okHttpClient= OkHttpClient.Builder().build()
         val request = Request.Builder()
             .url ("http://api.github.com/users")
             .build()
@@ -36,26 +47,28 @@ class UsersPresenter(
             }
             override fun onFailure(call: Call, e: IOException) {
             }
-        })
+        })*/
+                //#endregion
 
-    }
+            }
 
-    fun onUserClicked(userModel: GithubUserModel) {
-        router.navigateTo(AppScreens.userDetailScreen(userModel.login))
-        router.navigateTo(AppScreens.imageConverter())
-    }
+        fun onUserClicked(userModel: GithubUserModel) {
+            router.navigateTo(AppScreens.userDetailScreen(userModel.login))
+            router.navigateTo(AppScreens.imageConverter())
+        }
 
-    private fun handleUsers(users: List<GithubUserModel>) {
-        viewState.updateList()
-    }
+        fun handleUsers(users: List<GithubUserModel>) {
+            viewState.updateList()
+        }
 
 
-    fun onDestroy(view: UsersView?) {
-        disposables.dispose()
-    }
+        fun onDestroy(view: UsersView?) {
+            disposables.dispose()
+        }
 
-    fun backPressed(): Boolean {
-        router.exit()
-        return true
+        fun backPressed(): Boolean {
+            router.exit()
+            return true
+        }
     }
 }
